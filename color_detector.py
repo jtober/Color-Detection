191f83231c394b04d15374c6fb0f791b0a83c297
Julia Tober <jtober@ucsc.edu>
2020-10-23 16:02:11 -0700

    First draft

--- color_detector.py
+++ color_detector.py
@@ -8,10 +8,10 @@ import argparse
 ap = argparse.ArgumentParser()
 ap.add_argument('-i','--image', required=True, help="Image Path")
 args= vars(ap.parse_args())
-image_path = args['image']
+img_path = args['image']
 
 #using opencv to read the image 
-img = cv2.imread(image_path) #loads image from specific file, in this case it'll be from the terminal
+img = cv2.imread(img_path) #loads image from specific file, in this case it'll be from the terminal
 
 #global variables
 clicked = False
@@ -32,4 +32,38 @@ def getColorName(R,G,B):
             cname= csv.loc[i, "color_name"]
         return cname
 
-#
+#function to get x,y coordinates of mouse double click and calculate rbg values
+def clicked_mouse(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b,g,r,xpos,ypos, clicked
+        clicked = True
+        xpos = x
+        ypos = y
+        b,g,r = img[y,x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_funcion)
+
+while(True):
+    cv2.show('image',img)
+    if (clicked):
+        #create rectangle showing image 
+        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
+        #create text to display the color name and its RGB values
+        text = getColorName(r,g,b) + ' R='+ str(r) + ' G='+ str(g) + 'B='+ str(b)
+
+        cv2.putText(img, text, (50,50), 2,0.8,(255,255,255),2,cv2.LINE_AA)
+
+        #making sure that light colors will display properly
+        if (r+g+b>=600):
+            cv2.putText(img, text, (50,50), 2,0.8,(0,0,0),2,cv2.LINE_AA)
+
+        clicked = False
+    #break loop if user presses 'esc' key
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows
